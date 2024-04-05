d494c57e35e547fe6b785ba3c7053e883bb1eb0b
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-05 16:33:41 +0300

    geliştiriliyor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/todo';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PrimeNg TodoApp';
+  task='';
   todos = [
     {
       id: 1,
       task: 'first todo',
+      completed: true
+    },
+    {
+      id: 2,
+      task: 'second todo',
       completed: false
     }
   ]
+  updateTodo(e: unknown, todo: Todo) {
+    console.log(e, todo)
+  }
+  deleteTodo(e:unknown,id:Todo['id']){
+console.log(e,id);
+  }
+  addTodo(){
+    console.log('added',this.task)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CardModule,
+    TableModule,
+    CheckboxModule,
     FormsModule,
-    TableModule
+    ButtonModule,
+    InputTextModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
